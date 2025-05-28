3eafd68bd10b01b7f1c65909c1cc41ab64a38495
diarboy <bigdickdumb@gmail.com>
2025-05-28 09:51:07 +0700

    firebase .env

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,20 +1,15 @@
-// Import the functions you need from the SDKs you need
+// src/firebase.ts
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: 'AIzaSyAXXVSIjF1NlMzB9BUmds6VvHPrioA11EQ',
-  authDomain: 'crud-app-acdc8.firebaseapp.com',
-  projectId: 'crud-app-acdc8',
-  storageBucket: 'crud-app-acdc8.firebasestorage.app',
-  messagingSenderId: '361329255865',
-  appId: '1:361329255865:web:d8ad5bf7ceebe337d54268',
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
